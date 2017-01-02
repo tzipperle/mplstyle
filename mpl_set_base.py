c7aa16a2ec56010cce67c5270825aa2db2754f1e
Thomas Zipperle <thomas.zipperle@tum.de>
2017-01-02 10:21:17 +0100

    added generate custom color maps

--- mpl_set_base.py
+++ mpl_set_base.py
@@ -1,4 +1,6 @@
+import sys
 import matplotlib as mpl
+import numpy as np
 from cycler import cycler
 
 
@@ -214,3 +216,47 @@ class PlotBase:
 
     def _set_plt_style(self, style, colors, prop_cycle_colors):
         self._set_default_plt_style()
+
+    def get_cmap(self, colors, position=None, bit=False):
+        """ Generate custom color maps for Matplotlib.
+
+        The method allows you to create a list of tuples with 8-bit (0 to 255)
+        or arithmetic (0.0 to 1.0) RGB values to create linear color maps.
+        Arrange your tuples so that the first color is the lowest value for
+        the color bar and the last is the highest.
+
+        Args:
+            colors: list of RGB tuples with 8-bit (0 to 255) or
+                    arithmetic (0 to 1); default: arithmetic
+            position: contains a list from 0 to 1 to dictate the location
+                      of each color
+            bit: boolean; default: False (arithmetic); True (RGB)
+
+        Returns:
+            cmap: a color map with equally spaced colors
+
+        Example:
+            >>> cmap = get_cmap(colors=[(255, 0, 0), (0, 157, 0),)], bit=True)
+            >>> cmap = get_cmap([(1, 1, 1), (0.5, 0, 0)], position=[0, 1]))
+        """
+
+        bit_rgb = np.linspace(0, 1, 256)
+        if position is None:
+            position = np.linspace(0, 1, len(colors))
+        else:
+            if len(position) != len(colors):
+                sys.exit("position length must be the same as colors")
+            elif position[0] != 0 or position[-1] != 1:
+                sys.exit("position must start with 0 and end with 1")
+        if bit:
+            for i in range(len(colors)):
+                colors[i] = (bit_rgb[colors[i][0]],
+                             bit_rgb[colors[i][1]],
+                             bit_rgb[colors[i][2]])
+        cdict = {'red': [], 'green': [], 'blue': []}
+        for pos, color in zip(position, colors):
+            cdict['red'].append((pos, color[0], color[0]))
+            cdict['green'].append((pos, color[1], color[1]))
+            cdict['blue'].append((pos, color[2], color[2]))
+
+        return mpl.colors.LinearSegmentedColormap('my_colormap', cdict, 256)
