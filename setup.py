7607991dae2e9055b1b1a8b99a5f9a1d66ff1ed8
Thomas Zipperle <thomas.zipperle@tum.de>
2017-08-04 08:25:18 +0200

    added setup.py and requirements.yaml

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='mpl_style',
+    version='v0.1',
+    packages=['mpl_style'],
+    url='https://github.com/tzipperle/mpl-style',
+    license='GNU General Public License v3.0',
+    author='Thomas Zipperle',
+    author_email='',
+    description='Automation of changing plot settings in matplotlib'
+)
