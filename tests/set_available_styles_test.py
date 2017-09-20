48d0693e5575b6204c637d54d16f0e8c4b4c0913
Thomas Zipperle <thomas.zipperle@tum.de>
2017-09-20 17:54:02 +0200

    added test check available style

---
+++ tests/set_available_styles_test.py
@@ -0,0 +1,30 @@
+import pytest
+from mplstyle.base import PLTbase
+from mplstyle.databay import PLTdatabay
+from mplstyle.dynamis import PLTdynamis
+from mplstyle.enfo import PLTenfo
+from mplstyle.ewk import PLTewk
+from mplstyle.tz import PLTtz
+
+
+@pytest.mark.parametrize('used_class',
+                         [PLTtz(), PLTewk(), PLTenfo(), PLTdynamis(),
+                          PLTdatabay(), PLTbase()])
+@pytest.mark.xfail(raises=NotImplementedError)
+def test_available_styles(used_class):
+    available_styles = used_class._styles_available
+
+    # test plt_style
+    if 'plt_style' in available_styles:
+        for style in available_styles['plt_style']:
+            used_class.set_style(plt_style=style)
+
+    # test color_style
+    if 'color_style' in available_styles:
+        for style in available_styles['color_style']:
+            used_class.set_style(color_style=style)
+
+    # test color_order_style
+    if 'color_order_style' in available_styles:
+        for style in available_styles['color_order_style']:
+            used_class.set_style(color_order_style=style, color_style=style)
