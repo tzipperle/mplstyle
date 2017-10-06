a2c9d37e378726eee396a8f8121efbadbedaa8db
Thomas Zipperle <t@zipperle.biz>
2017-10-06 22:28:18 +0200

    code refactoring and mpl2.1

--- examples/fuel_bar_plot.py
+++ examples/fuel_bar_plot.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-import matplotlib as mpl
 import pandas as pd
+import matplotlib.pyplot as plt
 from mplstyle.ewk import PLTewk
 
 
@@ -10,41 +9,29 @@ def read_data(name):
 
 
 def plot_fig(df, title):
+    fig, (ax0, ax1) = plt.subplots(2, 1, figsize=[11, 8],
+                                   gridspec_kw={'height_ratios': [1, 0.1]})
+    ax1.axis('off')
+
     colors = ewk_plt.get_colors()
-
-    gs = mpl.gridspec.GridSpec(2, 1, height_ratios=[1, 0.1])
-    fig = plt.figure(figsize=[11, 8])
-    ax = fig.add_subplot(gs[0])
-
     color = [colors[i] for i in df.columns]
-    df.plot.bar(ax=ax, stacked=True, width=0.5, edgecolor=None,
+    df.plot.bar(ax=ax0, stacked=True, width=0.5, edgecolor=None,
                 zorder=2, rot=0, legend=False, color=color)
 
-    ax.set_xlabel('').set_visible(False)
+    handles, labels = ax0.get_legend_handles_labels()
+    ax0.legend(handles, labels, loc='upper center', ncol=3,
+               bbox_to_anchor=(0.5, -0.04))
 
-    handles, labels = ax.get_legend_handles_labels()
-    ax.legend(handles, labels, loc='upper center', ncol=3,
-              bbox_to_anchor=(0.5, -0.04))
-
-    ax.set_title(title, fontsize=20, fontweight='bold')
+    ax0.set_title(title, fontsize=20, fontweight='bold')
+    ax0.set_xlabel('').set_visible(False)
     fig.tight_layout()
     plt.show()
-
-
-def _x_axis_tick(ax, n=5):
-    ticks = ax.xaxis.get_ticklocs()
-    ticklabels = [l.get_text() for l in ax.xaxis.get_ticklabels()]
-    ax.xaxis.set_ticks(ticks[::n])
-    ax.xaxis.set_ticklabels(ticklabels[::n])
-    return ax
 
 
 if __name__ == '__main__':
     ewk_plt = PLTewk()
     ewk_plt.set_style('ewk')
 
-    name_input_file = 'ghd_bs'
-    title = 'GHD Endenergieverbrauch in PJ nach Brennstoffen'
+    data = read_data('ghd_bs.xlsx')
 
-    data = read_data('{}.xlsx'.format(name_input_file))
-    plot_fig(data, title=title)
+    plot_fig(data, title='GHD Endenergieverbrauch in PJ nach Brennstoffen')
